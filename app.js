f82ba39515ee0a1912cb753f09ccdb8215823f27
MrRedbloX <charles.cogoluegnes@gmail.com>
2020-10-12 16:07:33 +0200

    Commit

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const {MongoClient, ObjectId} = require('mongodb')
+const uri = "mongodb+srv://admin:admin@cluster0.oddle.mongodb.net/Unity?retryWrites=true&w=majority";
+
 app.get('/', (req, res) => {
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://admin:admin@cluster0.oddle.mongodb.net/Unity?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   client.connect(err => {
-	   const collection = client.db("Unity").collection("Cubes");
+	   collection = client.db("Unity").collection("Cubes");
 	   collection.find().toArray((err, cubes) => { 
 		res.send(cubes);
 		client.close();
 	  }
 		  )
   });
+})
+
+app.get('/id', (req, res) => {
+	if(req.query.id) {
+	 client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		client.connect(err => {
+	   collection = client.db("Unity").collection("Cubes");
+	   try {
+		   collection.findOne({'_id': ObjectId(req.query.id) }, (err, cube) => {
+			 if(err) throw err
+			 res.send(cube)
+		  })
+	   }
+	   catch (e) {
+		   res.send({x: 0, y: 0, z: 0})
+	   }
+	   
+	   finally {
+		   client.close()
+	   }
+  });
+	}
 })
 
 app.listen(port, () => {
